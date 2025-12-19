2e44b165478bf852ae23c9fa036e02d915968038
Adam <adam.hachicha@student-cs.fr>
2025-12-19 20:01:51 +0100

    get latest date in db

--- etl/data_fetching.py
+++ etl/data_fetching.py
@@ -21,11 +21,34 @@ DB_PASS = os.getenv("DB_PASS", "postgres")
 DB_PORT = os.getenv("DB_PORT", "5433")
 
 
-def update_db():
+def get_latest_date_in_db():
+    try:
+        logger.info(
+            "Connecting to the PostgreSQL database to get the latest date...")
+        connection = psycopg2.connect(
+            user=DB_USER,
+            password=DB_PASS,
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_NAME
+        )
+        cursor = connection.cursor()
+        query = "SELECT MAX(trading_date) FROM market_data;"
+        cursor.execute(query)
+        result = cursor.fetchone()
+        latest_date = result[0] if result[0] is not None else '01/01/2016'
+        logger.info(f"Latest date in DB: {latest_date}")
+        return latest_date.strftime("%d/%m/%Y")
+    except Exception as e:
+        logger.error(f"Error while fetching latest date from DB: {e}")
+        return '01/01/2016'
+
+
+def update_db(start_date='01/01/2016', end_date='01/01/2021'):
     try:
         logger.info("Fetching historical crypto data from investpy...")
         df = investpy.get_crypto_historical_data(
-            crypto='bitcoin', from_date='01/01/2016', to_date='01/01/2021')
+            crypto='bitcoin', from_date=start_date, to_date=end_date)
         return df
     except Exception as e:
         logger.error(f"Error while fetching data from investpy: {e}")
