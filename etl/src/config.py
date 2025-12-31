a08bc04df6de97381b37d3ff60fd00bc9622deda
Adam <adam.hachicha@student-cs.fr>
2025-12-31 19:44:06 +0100

    config file added

---
+++ etl/src/config.py
@@ -0,0 +1,17 @@
+import os
+
+
+def pull_db_config():
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_NAME = os.getenv("DB_NAME", "postgres")
+    DB_USER = os.getenv("DB_USER", "postgres")  # Default user
+    DB_PASS = os.getenv("DB_PASS", "postgres")
+    DB_PORT = os.getenv("DB_PORT", "5433")
+
+    return {
+        "host": DB_HOST,
+        "name": DB_NAME,
+        "user": DB_USER,
+        "pass": DB_PASS,
+        "port": DB_PORT
+    }
