95bf9697a4743cd0b78c80e8ceabcbaf24736b6e
Adam <adam.hachicha@student-cs.fr>
2026-01-03 15:52:32 +0100

    get_db_connection used in all update_db functions

--- etl/src/update_db.py
+++ etl/src/update_db.py
@@ -5,11 +5,13 @@ import pandas as pd
 import psycopg2
 from psycopg2 import extras
 from config import pull_db_config
+from contextlib import contextmanager
 
 logger = logging.getLogger(__name__)
 db_config = pull_db_config()
 
 
+@contextmanager
 def get_db_connection():
     try:
         connection = psycopg2.connect(
@@ -35,22 +37,14 @@ def get_latest_date_in_db():
     try:
         logger.info(
             "Connecting to the PostgreSQL database to get the latest date...")
-        connection = psycopg2.connect(
-            user=db_config["user"],
-            password=db_config["pass"],
-            host=db_config["host"],
-            port=db_config["port"],
-            database=db_config["name"]
-        )
-        cursor = connection.cursor()
-        query = "SELECT MAX(trading_date) FROM market_data;"
-        cursor.execute(query)
-        result = cursor.fetchone()
-        latest_date = result[0] if result[0] is not None else '01/01/2016'
-        logger.info(f"Latest date in DB: {latest_date}")
-        cursor.close()
-        connection.close()
-        return latest_date.strftime("%Y-%m-%d")
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                query = "SELECT MAX(trading_date) FROM market_data;"
+                cursor.execute(query)
+                result = cursor.fetchone()
+                latest_date = result[0] if result[0] is not None else '01/01/2016'
+                logger.info(f"Latest date in DB: {latest_date}")
+                return latest_date.strftime("%Y-%m-%d")
     except Exception as e:
         logger.error(f"Error while fetching latest date from DB: {e}")
         return '2016-01-01'
@@ -67,37 +61,28 @@ def update_db(data_path):
     df = pd.read_parquet(data_path)
 
     try:
-        connection = psycopg2.connect(
-            user=db_config["user"],
-            password=db_config["pass"],
-            host=db_config["host"],
-            port=db_config["port"],
-            database=db_config["name"]
-        )
-        cursor = connection.cursor()
-        df_clean = df.reset_index()
-        data_list = [tuple(x) for x in df_clean.to_numpy()]
-        logger.info("Inserting new data into the database...")
-        query = """
-        INSERT INTO market_data (
-                trading_date, 
-                open_price, 
-                high_price, 
-                low_price, 
-                close_price, 
-                volume
-            )
-        VALUES %s
-        ON CONFLICT (trading_date) 
-        DO UPDATE SET 
-            trading_date = EXCLUDED.trading_date;
-        """
-        extras.execute_values(cursor, query, data_list)
-        connection.commit()
-        cursor.close()
-        connection.close()
-        logger.info(
-            f"Database update complete. Added {len(data_list)} records.")
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                df_clean = df.reset_index()
+                data_list = [tuple(x) for x in df_clean.to_numpy()]
+                logger.info("Inserting new data into the database...")
+                query = """
+                INSERT INTO market_data (
+                        trading_date, 
+                        open_price, 
+                        high_price, 
+                        low_price, 
+                        close_price, 
+                        volume
+                    )
+                VALUES %s
+                ON CONFLICT (trading_date) 
+                DO UPDATE SET 
+                    trading_date = EXCLUDED.trading_date;
+                """
+                extras.execute_values(cursor, query, data_list)
+                logger.info(
+                    f"Database update complete. Added {len(data_list)} records.")
 
     except Exception as e:
         logger.error(f"Error while connecting to the database: {e}")
